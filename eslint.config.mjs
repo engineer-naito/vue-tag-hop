d8c1cd672fe3797be68294175a1247f668d1d941
engineer-naito <kosuke.naito.engineer@gmail.com>
2025-03-08 23:50:43 +0900

    fmt

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,30 +3,30 @@ import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 
 export default [{
-    files: ["**/*.ts"],
+  files: ["**/*.ts"],
 }, {
-    plugins: {
-        "@stylistic": stylistic,
-        "@typescript-eslint": typescriptEslint,
-    },
+  plugins: {
+    "@stylistic": stylistic,
+    "@typescript-eslint": typescriptEslint,
+  },
 
-    languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
-    },
+  languageOptions: {
+    parser: tsParser,
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
 
-    rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
+  rules: {
+    "@typescript-eslint/naming-convention": ["warn", {
+      selector: "import",
+      format: ["camelCase", "PascalCase"],
+    }],
 
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        "@stylistic/semi": ["error", "always"], 
-        "@stylistic/quotes": ["error", "double"],
-        "@stylistic/indent": ["error", 2],
-    },
+    curly: "warn",
+    eqeqeq: "warn",
+    "no-throw-literal": "warn",
+    "@stylistic/semi": ["error", "always"], 
+    "@stylistic/quotes": ["error", "double"],
+    "@stylistic/indent": ["error", 2],
+  },
 }];
